df37b4d32ca5bd50940053f0f6bd216c4de758bc
nicola1997 <n.dipinto10@gmail.com>
2023-02-27 06:57:14 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         ArrayList <Squadra> squadreA= new ArrayList<>();
 
-        for (int i=0;i<20;i++){
+        for (int i=0;i<nomiSquadre.length;i++){
             squadreA.add(new Squadra(nomiSquadre[i]));
         }
         Campionato c= new Campionato("serieA",squadreA);
